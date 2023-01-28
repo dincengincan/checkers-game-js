bb61d5b786587cc6c3379de65ec1329d5e4f234e
Engin Can Dinç <engincandinc@gmail.com>
2023-01-28 20:03:05 +0300

    Add basic beating mechanism

--- script.js
+++ script.js
@@ -30,12 +30,13 @@ const whitePieces = new Set([
 ]);
 
 const blackPieces = new Set([
+  "35",
   "51",
   "53",
   "55",
   "57",
   "60",
-  "62",
+
   "64",
   "66",
   "71",
@@ -135,45 +136,88 @@ function playMove() {
     (isWhitePieceSelected ? whitePieces : blackPieces).add(nextLocation);
     (isWhitePieceSelected ? whitePieces : blackPieces).delete(previousLocation);
 
+    if (hasBeaten(previousLocation, nextLocation)) {
+      beatPieces(previousLocation, nextLocation);
+    }
+
     selectedPieceElement.classList.remove("selected");
     selectedPieceElement = null;
     isWhiteNext = !isWhiteNext;
   }
 }
 
+function hasBeaten(prevLocation, nextLocation) {
+  const nextNeighbor = getNextNeighbour(prevLocation, whiteNeighbourCalculator);
+  return nextLocation !== nextNeighbor;
+}
+
+function beatPieces(prevLocation, nextLocation) {}
+
 function isValidMove(prevLocation, nextLocation) {
-  const [x, y] = prevLocation.split("");
-  const numberX = Number(x);
-  const numberY = Number(y);
-
-  const validMoves = new Set();
-
-  let firstValidX = 0;
-  let firstValidY = 0;
-  let secondValidX = 0;
-  let secondValidY = 0;
-
-  if (isWhitePieceSelected) {
-    firstValidX = numberX + 1;
-    firstValidY = numberY - 1;
-
-    secondValidX = numberX + 1;
-    secondValidY = numberY + 1;
-  } else {
-    firstValidX = numberX - 1;
-    firstValidY = numberY - 1;
-
-    secondValidX = numberX - 1;
-    secondValidY = numberY + 1;
-  }
-
-  const firstValidLocation = `${firstValidX}${firstValidY}`;
-  const secondValidLocation = `${secondValidX}${secondValidY}`;
-
-  validMoves.add(firstValidLocation);
-  validMoves.add(secondValidLocation);
+  const validMoves = validateNeighbours(prevLocation);
 
   return validMoves.has(nextLocation);
+}
+
+function validateNeighbours(pieceLocation, moves) {
+  const validMoves = moves ?? new Set();
+  const nextNeighbour = getNextNeighbour(
+    pieceLocation,
+    whiteNeighbourCalculator
+  );
+
+  const nextNextNeighbour = getNextNeighbour(
+    nextNeighbour,
+    whiteNeighbourCalculator
+  );
+
+  if (whitePieces.has(nextNeighbour)) {
+    //if there is white return
+    return validMoves;
+  }
+
+  if (isCellEmpty(nextNeighbour)) {
+    // if empty add to valid moves and return
+    validMoves.add(nextNeighbour);
+    return validMoves;
+  }
+
+  if (blackPieces.has(nextNeighbour) && isCellEmpty(nextNextNeighbour)) {
+    // if there is opponent and space behind it add it
+    validMoves.add(nextNextNeighbour);
+  }
+
+  return validMoves;
+}
+
+function isCellEmpty(location) {
+  return !blackPieces.has(location) && !whitePieces.has(location);
+}
+
+function getNextNeighbour(location, calculator, direction) {
+  // TODO this abstraction is not working
+  return calculator(location, direction);
+}
+
+function whiteNeighbourCalculator(location, direction = "l") {
+  const [numberX, numberY] = extractLocationAsNumbers(location);
+
+  const firstNeightborX = numberX + 1;
+  const firstNeightborY = numberY - 1;
+
+  const secondNeighborX = numberX + 1;
+  const secondNeighborY = numberY + 1;
+
+  return direction === "l"
+    ? `${firstNeightborX}${firstNeightborY}`
+    : `${secondNeighborX}${secondNeighborY}`;
+}
+
+function extractLocationAsNumbers(location) {
+  const [x, y] = location.split("");
+  const numberX = Number(x);
+  const numberY = Number(y);
+  return [numberX, numberY];
 }
 
 function getLocation(element) {
